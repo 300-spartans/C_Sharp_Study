7874a8b5344f310f503da6340328eae4b84f3f2b
Sam <swright@tmcdesign.com>
2018-12-20 15:37:53 -0700

    First Session

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            string myName = "Sam";
+
+            string message = "My name is " + myName;
+
+            message.ToUpper();
+
+            Console.WriteLine(message);
+            Console.Read();
+
+        }
+
+        
+    }
+}
