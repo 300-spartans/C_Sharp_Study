c32b4f87a54713c48672328ca61c8904a6a2e181
Sam <swright@tmcdesign.com>
2019-01-07 12:05:52 -0700

    Session 5b

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,35 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace Average
 {
     class Program
     {
-        static int highScore = 8900;
-        static string highscorePlayer = "300spartans";
 
         static void Main(string[] args)
         {
-            int lengthOfText = 0;
-
-            for (int counter = 1; counter <= 20; counter+=2)
-            {
-                Console.WriteLine(counter);
-                Console.WriteLine("Counter incremented");
-            }
-
-            do
-            {
-                Console.WriteLine("Please enter the name of a friend:");
-                string name = Console.ReadLine();
-                int currentLength = name.Length;
-                lengthOfText += currentLength;
-            } while (lengthOfText < 20);
-
+            int average = GetAverage();
+            Console.WriteLine("The average is {0}", average);
             Console.Read();
-            
         }
 
-        
+        public static int GetAverage()
+        {
+            int totalScores = 0;
+            int numberOfScores = 0;
+            int numInput = 0;
+
+            while (numInput != -1)
+            {
+                Console.WriteLine("Please enter score (between 0 and 20)" +
+                "\nEnter -1 to get average");
+                Console.WriteLine("Current number of entries: {0}", numberOfScores);
+
+                string input = Console.ReadLine();
+                Console.WriteLine("------------------------------------");
+
+                if (!int.TryParse(input, out numInput)) 
+                {
+                    Console.WriteLine("Enter valid number score");
+                    continue;
+
+                } else if (numInput > 20)
+                {
+                    Console.WriteLine("Enter valid score");
+                    continue;
+                } else if (numInput == -1)
+                {
+                    break;
+                }
+
+                totalScores += numInput;
+                numberOfScores++;
+            }
+            int average = totalScores / numberOfScores;
+            return average;
+        }
     }
 }
